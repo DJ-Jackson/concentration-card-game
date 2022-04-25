5d88ffe43912ecc12db0bfa1813904baaaa94ffa
DJ-Jackson <jacdouglass@gmail.com>
2022-04-24 21:36:45 -0400

    Created Card struct

---
+++ structs/card.cpp
@@ -0,0 +1,11 @@
+//
+// Created by djjac on 4/24/2022.
+//
+
+struct Card
+{
+    int rank;
+    char suit;
+    bool is_flipped = false;
+    bool is_removed = false;
+};
--- structs/player.cpp
+++ structs/player.cpp
@@ -1,3 +1,11 @@
 //
 // Created by djjac on 4/24/2022.
 //
+#include <iostream>
+
+struct Player
+{
+    std::string name;
+    int match_count{}; // defaults match_count to 0 for each initialization
+    bool is_winner = false; // defaults is_Winner to false for each initialization
+};
