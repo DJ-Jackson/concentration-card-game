936af89342e6a83e92cf8f7eb2aa9997f2d20789
DJ-Jackson <jacdouglass@gmail.com>
2022-04-26 16:32:05 -0400

    Added constants

---
+++ constants/suit.h
@@ -0,0 +1,10 @@
+//
+// Created by djjac on 4/26/2022.
+//
+
+#ifndef CONCENTRATION_CARD_GAME_SUIT_H
+#define CONCENTRATION_CARD_GAME_SUIT_H
+
+char SUIT[] = {'H', 'D', 'S', 'C'};
+
+#endif //CONCENTRATION_CARD_GAME_SUIT_H
