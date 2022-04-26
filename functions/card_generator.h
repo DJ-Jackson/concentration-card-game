0e600755e2982a7a498f25b5c3a3971fc4e737d8
KingTae2002 <devontaepatterson0@gmail.com>
2022-04-26 17:37:53 -0400

    Created Card Generator Function

---
+++ functions/card_generator.h
@@ -0,0 +1,32 @@
+//
+// Created by Devontae Patterson on 4/25/22.
+//
+
+#ifndef CONCENTRATION_CARD_GAME_CARD_GENERATOR_H
+#define CONCENTRATION_CARD_GAME_CARD_GENERATOR_H
+
+
+#include <random>
+#include <ostream>
+#include <iostream>
+#include "../structs/card.h"
+#include "../constants/suit.h"
+
+Card card_randomizer()
+{
+    Card object;
+
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist13(1,13); // distribution in range [1, 13]
+    object.rank = dist13(rng);
+
+    std::uniform_int_distribution<std::mt19937::result_type> dist3(0,3); // distribution in range [0, 3]
+    object.suit = SUIT[dist3(rng)];
+
+    return object;
+}
+
+
+
+#endif //CONCENTRATION_CARD_GAME_CARD_GENERATOR_H
