a122d73ada3a387842b4da7d46a9b3f0f75f6f33
DJ-Jackson <jacdouglass@gmail.com>
2022-04-27 11:54:10 -0400

    Created function to create list of players

---
+++ functions/createplayerlist.h
@@ -0,0 +1,34 @@
+//
+// Created by djjac on 4/27/2022.
+//
+
+#ifndef CONCENTRATION_CARD_GAME_CREATEPLAYERLIST_H
+#define CONCENTRATION_CARD_GAME_CREATEPLAYERLIST_H
+
+#include "../structs/player.h"
+
+#include <vector>
+
+std::vector<Player> create_player_list()
+{
+    std::vector<Player> player_list;
+    int num_players;
+    std::string player_name;
+    do
+    {
+        std::cout << "How many people are playing? -> ";
+        std::cin >> num_players;
+    } while (num_players < 2 || 8 < num_players);
+
+    for (int i = 0; i < num_players; ++i)
+    {
+        std::cout << "What is player " << i + 1 << "'s name? -> ";
+        std::getline(std::cin, player_name);
+
+        Player curr;
+        curr.name = player_name;
+        player_list.push_back(curr);
+    }
+}
+
+#endif //CONCENTRATION_CARD_GAME_CREATEPLAYERLIST_H
